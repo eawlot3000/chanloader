5a74eb07cff3c894be040b38b3d9659921cef52c
eawlot3000 <eawlot3000@gmail.com>
2022-07-11 11:55:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+###you love [4chan](https://www.4chan.org)? but gotta download each file with so many clicks? nahhhh
+
+###chanloader is a tool that help you download all the visual content of your fond channel
+
+###requirements:
+```
+pip3 install requests && pip3 install bs4 && pip3 install os && python3 -V
+```
+
+###usage
+```
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import os
+
+chan = input('\ngive me a base channel\neg: https://boards.4chan.org/s/\n\n--> ')
+howmanypages = int(input('\nhow many pages do you want?\neg1: 1 (only the first page)\neg2: 10 (download all from page1 to page10)\n\n--> '))
+
+def main():
+  r = requests.get(url)
+  html = bs(r.content, 'html.parser')
+  num, ret = 0, []
+  for link in html.find_all('a'):
+    link = link.get('href')
+    if 'i.4cdn.org' in link and num%2==0:
+      ret.append(link)
+    num += 1
+  
+  for i in range(len(ret)):
+    #TODO: for each page create a directory with page name to store files
+    filename = str(ret[i][13:].replace('/', '_'))
+    if os.path.isfile('%s'%filename):
+      print('\n%s'%filename, 'exists! you may have downloaded this link already\n')
+    else:
+      r = requests.get(str("https:") + ret[i])
+      with open('%s'%filename, 'wb') as f:
+        for image_link in r:
+          f.write(image_link)
+      print("--> new download succeed!\n*filename:", filename, "\n*filesize:",
+          int(os.stat(filename).st_size)/1000000, "MB", "\nready to download next one.\n")
+
+if howmanypages != 1:
+  for i in range(howmanypages+1):
+    url = str(chan) + str(i+1)
+    main()
+else:
+  url = str(chan)
+  main()
+print('*** all photos has been downloaded! ***\n')
